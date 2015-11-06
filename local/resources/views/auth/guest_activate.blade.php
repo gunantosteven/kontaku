068a69cddf6c7875721918a3565521f3e5d5d1d9
Steven Gunanto <gunantosteven@gmail.com>
2015-11-06 12:53:50 +0700

    edit typo

--- local/resources/views/auth/guest_activate.blade.php
+++ local/resources/views/auth/guest_activate.blade.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <div class="panel panel-default">
-        <div class="panel-heading">Home</div>
+        <div class="panel-heading">Activation</div>
         <div class="panel-body">
           <p>An email was sent to {{ $email }} on {{ $date }}.</p>
 
--- local/resources/views/auth/tooManyEmails.blade.php
+++ local/resources/views/auth/tooManyEmails.blade.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <div class="panel panel-default">
-        <div class="panel-heading">Home</div>
+        <div class="panel-heading">Activation</div>
         <div class="panel-body">
           <p>Too many activation emails have been sent to {{ $email }}.</p>
         </div>
