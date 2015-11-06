72c4dec55e432785eb0504ef3360ba7de9493394
Steven Gunanto <gunantosteven@gmail.com>
2015-11-06 12:36:49 +0700

    edit messages

--- local/resources/views/emails/activateAccount.blade.php
+++ local/resources/views/emails/activateAccount.blade.php
@@ -1,4 +1,9 @@
-Click here to activate your account: 
+"Click on the link below to activate your account: 
+<br>
+<br>
 <a href="{{ url('activate/'.$code) }}" >
 {{ url('activate/'.$code) }}
 </a>
+<br>
+<br>
+Please do not reply this email. Thanks!"
--- local/resources/views/emails/password.blade.php
+++ local/resources/views/emails/password.blade.php
@@ -1,1 +1,9 @@
-Click here to reset your password: {{ url('password/reset/'.$token) }}
+"Click on the link below to reset your password: 
+<br>
+<br>
+<a href="{{ url('password/reset/'.$token) }}" >
+{{ url('password/reset/'.$token) }}
+</a>
+<br>
+<br>
+If you don't want to reset your password, just ignore this email. Thanks!"
