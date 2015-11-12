5f30580ef36a4c9701cf3251638c79b755c27047
Steven Gunanto <gunantosteven@gmail.com>
2015-11-12 14:14:35 +0700

    add meta tag in page home

--- local/resources/views/user/home.blade.php
+++ local/resources/views/user/home.blade.php
@@ -4,6 +4,7 @@
         <title>KontaKKu</title>    
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">   
         <meta name="csrf-token" content="{{ csrf_token() }}" />    
+        <meta name="robots" content="noindex">
         <!-- JavaScript -->
         <script type='text/javascript' src={{ asset('js/jquery-1.11.1.min.js') }}></script>
         <script type='text/javascript' src={{ asset('js/jquery.mobile-1.4.5.min.js') }}></script>
