4d9f3ee4450f920bc9ae6dbdd95cca45ca052489
Steven Gunanto <gunantosteven@gmail.com>
2015-11-23 22:03:59 +0700

    bug fix name field

--- local/resources/views/app.blade.php
+++ local/resources/views/app.blade.php
@@ -38,7 +38,7 @@
 								    <li class="{{ Request::is('auth/register') ? 'active' : '' }}"><a href={{ url("/auth/register") }}>Register</a></li>
 							    @else
 							    	<li class="dropdown">
-										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->fullname }} <span class="caret"></span></a>
 										<ul class="dropdown-menu" role="menu">
 											<li><a href={{ url("/auth/logout") }}>Logout</a></li>
 										</ul>
