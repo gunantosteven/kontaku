b9f58242930c5ecf044b3f8bd554b191c9f32568
Steven Gunanto <gunantosteven@gmail.com>
2015-11-27 18:46:44 +0700

    add favicon.ico in home page

--- local/resources/views/user/home.blade.php
+++ local/resources/views/user/home.blade.php
@@ -2,6 +2,7 @@
 <html>      
     <head>      
         <title>KontaKKu</title>    
+        <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">   
         <meta name="csrf-token" content="{{ csrf_token() }}" />    
         <meta name="robots" content="noindex">
