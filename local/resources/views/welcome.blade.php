9be78941861c7e3191d1cfff87f44481d885c12a
Steven Gunanto <gunantosteven@gmail.com>
2016-01-04 09:46:41 +0700

    add ga

--- local/resources/views/welcome.blade.php
+++ local/resources/views/welcome.blade.php
@@ -10,6 +10,15 @@
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 	</script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-67268716-2', 'auto');
+	  ga('send', 'pageview', 'welcome');
+	</script>
 	<div class="slider">		
 		  <center><img src="img/logo.png" alt="" class="img-responsive" height="683" width="406" /></center> 		
 	</div>	
