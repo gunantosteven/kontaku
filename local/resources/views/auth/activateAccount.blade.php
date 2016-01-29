c2d2d9a2f0ab861e0ac51d6286716462f9048802
Steven Gunanto <gunantosteven@gmail.com>
2016-01-29 12:17:23 +0700

    edit typo

--- local/resources/views/auth/activateAccount.blade.php
+++ local/resources/views/auth/activateAccount.blade.php
@@ -9,7 +9,7 @@
         <div class="panel-body">
           <p>We have sent an email to {{ $email }}.</p>
 
-		  <p>Please click the link in it to activate your account.</p>
+		  <p>Please check your spam folder just in case the confirmation email got delivered there instead of your inbox.</p>
         </div>
       </div>
     </div>
