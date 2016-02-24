18325a55c2a127306bb04388461df01f6f23a166
Steven Gunanto <gunantosteven@gmail.com>
2016-02-24 20:06:01 +0700

    add link fb

--- local/resources/views/app.blade.php
+++ local/resources/views/app.blade.php
@@ -65,7 +65,7 @@
 					<div class="col-sm-12 align-center">
 						<ul class="social-network social-circle">
 							<li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
-							<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="https://www.facebook.com/kontakku/" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
 							<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
 							<li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
 							<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
