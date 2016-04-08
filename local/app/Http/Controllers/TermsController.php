3bcdf8451eda40c6bf8ca13a2552154de3b8ed74
Steven Gunanto <gunantosteven@gmail.com>
2016-04-08 10:54:45 +0700

    delete middleware guest

--- local/app/Http/Controllers/TermsController.php
+++ local/app/Http/Controllers/TermsController.php
@@ -14,16 +14,6 @@ class TermsController extends Controller {
 	*/
 
 	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('guest');
-	}
-
-	/**
 	 * Show the application welcome screen to the user.
 	 *
 	 * @return Response
