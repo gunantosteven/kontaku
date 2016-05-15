0667c9154863f3b0bc5da29c71aee62a184cb442
Steven Gunanto <gunantosteven@gmail.com>
2016-05-15 11:45:44 +0700

    remove ads

--- local/resources/views/welcome.blade.php
+++ local/resources/views/welcome.blade.php
@@ -25,9 +25,8 @@
 	<!-- column content -->
 	<div id="main-content">
 		<div class="container">
-			<center>
+			<!-- <center>
 				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-				<!-- welcome_page -->
 				<ins class="adsbygoogle"
 				     style="display:block"
 				     data-ad-client="ca-pub-5254482915141018"
@@ -36,7 +35,7 @@
 				<script>
 				(adsbygoogle = window.adsbygoogle || []).push({});
 				</script>
-			</center>
+			</center> -->
 			<div class="row">
 				<div class="big-box">
 					<div class="col-lg-12">
